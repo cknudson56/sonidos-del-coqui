1774062aa432cb37d53c643e84cf915ae5706f62
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-06 16:33:17 -0600

    Add fonts and colors

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Lily+Script+One|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i&display=swap" rel="stylesheet"/>
   <title><?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
 </head>
