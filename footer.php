2c60504183f4135b8856884e2a8b58a4c691f36d
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-06 16:53:23 -0600

    Footer styling

--- footer.php
+++ footer.php
@@ -1,24 +1,26 @@
-<div class="container">
-  <div class="row">
-    <div class="twelve columns">
-      <?php wp_nav_menu(array(
-        'theme_location' => 'footer-menu',
-        'container-class' => 'menu-footer'
-        ));
-      ?>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="twelve columns">
+        <?php wp_nav_menu(array(
+          'theme_location' => 'footer-menu',
+          'container-class' => 'menu-footer'
+          ));
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns">
+        <?php dynamic_sidebar('middle-footer'); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns">
+        <p>Site Created by Christie Knudson</p>
+      </div>
     </div>
   </div>
-  <div class="row">
-    <div class="twelve columns">
-      <?php dynamic_sidebar('middle-footer'); ?>
-    </div>
-  </div>
-  <div class="row">
-    <div class="twelve columns">
-      <p>Site Created by Christie Knudson</p>
-    </div>
-  </div>
-</div>
+<footer>
 
 
 <?php wp_footer(); ?>
