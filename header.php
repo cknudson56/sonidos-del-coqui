911231bedebc6163902033782184c6fe79b5a10e
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-07 17:55:34 -0600

    Add fontawesome

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Lily+Script+One|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i&display=swap" rel="stylesheet"/>
   <title><?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
