1ccded880696ae58ebcb837ff6b67f60c54bc2b6
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-07 22:39:07 -0600

    Add a video banner page template

---
+++ page-video.php
@@ -0,0 +1,24 @@
+<?php /*Template Name: Video-Top Page Template*/ ?>
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="twelve columns head-margin-top">
+    </div>
+  </div>
+
+  <div class="row section-margins">
+    <div class="twelve columns">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <?php the_content();
+          }
+        }
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
