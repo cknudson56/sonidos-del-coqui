39576f619d832acfa27bf6dad666cbf5084ef444
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-07 23:18:35 -0600

    Add featured img

--- page.php
+++ page.php
@@ -12,6 +12,9 @@
         if(have_posts()){
           while(have_posts()){
             the_post();?>
+            <div class="post-feature">
+              <?php the_post_thumbnail(); ?>
+            </div>
             <h2 class="center-text section-margins"><?php the_title(); ?></h2>
             <?php the_content();
           }
