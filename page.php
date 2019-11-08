81ef79c5d14ba07413b32a7ffca0b2c0c947e9d4
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-11-07 23:20:39 -0600

    Adjust placement of img

--- page.php
+++ page.php
@@ -3,6 +3,16 @@
 <div class="container">
   <div class="row">
     <div class="twelve columns head-margin-top">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <div class="post-feature">
+              <?php the_post_thumbnail(); ?>
+            </div> <?
+          }
+        }
+      ?>
     </div>
   </div>
 
@@ -12,9 +22,6 @@
         if(have_posts()){
           while(have_posts()){
             the_post();?>
-            <div class="post-feature">
-              <?php the_post_thumbnail(); ?>
-            </div>
             <h2 class="center-text section-margins"><?php the_title(); ?></h2>
             <?php the_content();
           }
