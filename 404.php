bb3ec9a23f4761cdda044768403880d69940b3a6
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-12-30 13:57:32 -0600

    add section margins to 404

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
 <div class="container">
 <div class="row  head-margin-top">
-  <div class="twelve columns not-found">
+  <div class="twelve columns not-found section-margins">
     <h2>Page Not Found</h2>
     <h3>This usually never happens.</h3>
     <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
